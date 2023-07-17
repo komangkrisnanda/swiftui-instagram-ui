81cfe4a2eac6f3ff33712e492e7166d7ad115a67
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-17 21:49:19 +0800

    Initial Commit

---
+++ Shared/InstagramUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUIApp.swift
+//  Shared
+//
+//  Created by Komang Krisnanda on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
